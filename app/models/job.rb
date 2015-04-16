ff69c8a876e080b99990a4c9676ca7e980d58f91
dibenso <dibenso93@gmail.com>
2015-04-16 14:24:34 -0400

    add User and Admin models with devise

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,7 @@
 class Job < ActiveRecord::Base
+	validates :name, :min_wage, :max_wage, :time,
+			  :location, :description, presence: true
+
+	validates :description, length: { in: 32, 4096 }
+	validates :time, inclusion: { in: %w(Part Full Contract Seasonal) }
 end
