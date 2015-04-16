2d55a4e3109aa6186bc146a98cc56e7800788e03
dibenso <dibenso93@gmail.com>
2015-04-16 14:49:27 -0400

    add links to application layout

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,6 +2,6 @@ class Job < ActiveRecord::Base
 	validates :name, :min_wage, :max_wage, :time,
 			  :location, :description, presence: true
 
-	validates :description, length: { in: 32, 4096 }
+	validates :description, length: { in: 32..4096 }
 	validates :time, inclusion: { in: %w(Part Full Contract Seasonal) }
 end
